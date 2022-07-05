76c0e64be248d1558135cbb8e8773b9eb071a778
RobSilkImpres <robertsilk@impres.com>
2022-07-05 17:33:23 -0400

    adding alias

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+app = application
 
 @application.route('/')
 def hello_world():

74fc225dc46e4b59b723c038ef570c0b7a88d384
RobSilkImpres <robertsilk@impres.com>
2022-07-06 11:38:33 -0400

    Add in render_template

--- application.py
+++ application.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
+import requests
 
 # EB looks for an 'application' callable by default.
-application = Flask(__name__)
+application = Flask(__name__, template_folder="templates", static_folder="static")
 app = application
 
 @application.route('/')
-def hello_world():
-    return 'Sup'
+def index():
+    return render_template('index.html')
+
+# run the app.
+if __name__ == "__main__":
+    application.debug = True
+    application.run()

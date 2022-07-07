13ca39f3972aec36c59c80de70aebe0a7ca313bf
RobSilkImpres <robertsilk@impres.com>
2022-07-07 12:50:06 -0400

    Add input fields and alerts

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from datetime import date
 
 views = Blueprint('views', __name__)
 
@@ -6,6 +7,20 @@ views = Blueprint('views', __name__)
 def home():
     return render_template('index.html')
 
-@views.route('/addContact')
+@views.route('/addContact', methods=['GET', 'POST'])
 def add_contact():
+    if request.method == 'POST':
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        dob = request.form.get('dob')
+        
+        if not firstName :
+            flash('First name is required.', category='warning')
+        elif not lastName:
+            flash('Last name is required.', category='warning')
+        elif dob > date.today():
+            flash('Date of birth must be earlier than the current date.', category='warning')
+        else:
+            #add user to db
+            flash('Contact has been added.', category='success')
     return render_template('addContact.html')

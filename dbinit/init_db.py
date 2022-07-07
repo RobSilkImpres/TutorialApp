cef7f687bb2743e4033c355d26d82532aa803246
RobSilkImpres <robertsilk@impres.com>
2022-07-07 17:21:15 -0400

    Added DB initialization

--- dbinit/init_db.py
+++ dbinit/init_db.py
@@ -1,4 +1,9 @@
 from .dbc import transactionManager
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 def initializeDB():
+    logging.info("Beginning DB Initialization.")
     transactionManager.executeSQLFile('schema.sql')
+    logging.info("DB Initialization Complete.")

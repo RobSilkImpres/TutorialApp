f13487249c2d90972bad4d83712f319f6bdc192c
RobSilkImpres <robertsilk@impres.com>
2022-07-11 14:05:14 -0400

    Revert to previous method for logs

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from .logger import debug_file_handler, info_file_handler, error_file_handler
 from flask.logging import default_handler
 from .dbc import TransactionManager
 import os
@@ -10,12 +9,26 @@ def create_app():
     app = Flask(__name__, template_folder="templates", static_folder="static")
     app.config['SECRET_KEY'] = 'SECRET'
     #Adds the handler to the application
-    app.logger.removeHandler(default_handler)
+    logger = logging.getLogger(__name__)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+    app.logger.setLevel(logging.DEBUG)
+
+    debug_file_handler = logging.FileHandler(f'{os.environ.get("LOG_PATH", "log")}/debug.log')
+    info_file_handler = logging.FileHandler(f'{os.environ.get("LOG_PATH", "log")}/info.log')
+    error_file_handler = logging.FileHandler(f'{os.environ.get("LOG_PATH", "log")}/error.log')
+
+    debug_file_handler.setLevel(logging.DEBUG)
+    info_file_handler.setLevel(logging.INFO)
+    error_file_handler.setLevel(logging.ERROR)
+
+    debug_file_handler.setFormatter(formatter)
+    info_file_handler.setFormatter(formatter)
+    error_file_handler.setFormatter(formatter)
+    
     app.logger.addHandler(debug_file_handler)
     app.logger.addHandler(info_file_handler)
     app.logger.addHandler(error_file_handler)
-    
-    app.logger.setLevel(logging.DEBUG)
     
     app.logger.debug("Logger Attached.")
     

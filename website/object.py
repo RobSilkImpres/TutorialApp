ef56f7dc4c80f18040ddb6517313e182f4f1990d
RobSilkImpres <robertsilk@impres.com>
2022-07-11 18:02:48 -0400

    More loggers

--- website/object.py
+++ website/object.py
@@ -35,6 +35,7 @@ class Object(PersistanceManager):
     def commit(self):
         attributeMapping = {}
         for x in self.persistanceMapping.keys():
+            self.info("Getting Attribute " + x)
             attributeMapping[x] = getattr(self, x)
         if self.isNew == True:
             statement = self.createInsertStatement(attributeMapping)

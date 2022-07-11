da63403bb899ddc587a2b59e8d586fb9c8bbe20a
RobSilkImpres <robertsilk@impres.com>
2022-07-11 18:10:44 -0400

    Add view of contacts on home screen

--- website/views.py
+++ website/views.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from datetime import date
 from .entity import Entity
-
+from .persistance import PersistanceManager
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('index.html')
+    persist = PersistanceManager()
+    contacts = persist.runStatement('SELECT * FROM CONTACTS')
+    return render_template('index.html', contacts=contacts)
 
 @views.route('/addContact', methods=['GET', 'POST'])
 def add_contact():

1e4b2555e0bdc4b338ef6e3f7642164f6a7f2708
RobSilkImpres <robertsilk@impres.com>
2022-07-11 11:11:57 -0400

    Remove references to old engine package

--- application.py
+++ application.py
@@ -1,16 +1,12 @@
 from flask import Flask
 from werkzeug.exceptions import abort
 from website import create_app
-from initApp import engine
 
 # EB looks for an 'application' callable by default.
 application = create_app()
-eng = engine()
 app = application
 
 # run the app.
 if __name__ == "__main__":
     application.debug = True
-    eng.lambda_handler("Application starting")
-    eng.executeScript('schema.sql')
     application.run()
